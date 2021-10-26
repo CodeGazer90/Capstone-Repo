d2558f750468afbe4d25803fb38cc78a63f32b5e
Kenelle Moore <benevolentkstudios@gmail.com>
2021-10-26 18:58:59 -0400

    update

--- components/views/Play.js
+++ components/views/Play.js
@@ -1,12 +1,10 @@
 export default () => `
+
   <div id="blogspot">
   <h3>How are you feeling  about your journey today?</h3>
   <section>
-  <!-- HTML Codes by Quackit.com -->
-  <style type="text/css">
-  textarea.html-text-box {background-color:#000000;background-image:url(http://);background-repeat:no-repeat;background-attachment:fixed;border-width:1;border-style:solid;border-color:#00FF33;font-family:Cursive;font-size:14pt;color:#00FF33;}
-  input.html-text-box {background-color:#ffffff;font-family:Cursive;font-size:14pt;color:#000000;}
-  </style>
-  <form method="post" action="http://formspree.io/f/xoqyblvg"><textarea name="comments" cols="80" rows="20" class="html-text-box">How do you feel about your journey today?...</textarea><br><input type="submit" value="Submit" class="html-text-box"><input type="reset" value="Reset" class="html-text-box"></form>
+  <p>Blog your journey, post photos, and receive or offer words of encouragement from peers.</p>
+
+
   </section>
 </div>`;
